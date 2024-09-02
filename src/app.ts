54821526722ff5749c3eb41892dce4cb85d966e9
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2024-09-02 22:12:35 +0530

    feat: Cors middleware

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,19 @@ import globalErrorHandler from './middelware/globalErrorHandler'
 import responseMessage from './constant/responseMessage'
 import httpError from './utils/httpError'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const app: Application = express()
 
 // Middleware
 app.use(helmet())
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+        origin: ['https://client.com'],
+        credentials: true
+    })
+)
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
