bb29a7ba90939d63a37fbf50ef6b8806476bfed8
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2024-09-02 21:59:04 +0530

    feat: Helme.js setup

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -2,11 +2,24 @@ import { NextFunction, Request, Response } from 'express'
 import httpReponse from '../utils/httpReponse'
 import responseMessage from '../constant/responseMessage'
 import httpError from '../utils/httpError'
+import quicker from '../utils/quicker'
 
 export default {
     self: (req: Request, res: Response, next: NextFunction) => {
         try {
             httpReponse(req, res, 200, responseMessage.SUCCESS, { id: 1 })
+        } catch (err) {
+            httpError(next, err, req, 500)
+        }
+    },
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const healthData = {
+                application: quicker.getApplicationHealth(),
+                system: quicker.getSystemHealth(),
+                timeStamp: Date.now()
+            }
+            httpReponse(req, res, 200, responseMessage.SUCCESS, healthData)
         } catch (err) {
             httpError(next, err, req, 500)
         }
