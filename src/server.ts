8668e4173b5807f200bda6ad5b03b4cad3abc70d
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2024-09-02 21:08:41 +0530

    feat: Mongodb setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import app from './app'
 import config from './config/config'
+import databaseService from './service/databaseService'
 import logger from './utils/logger'
 
 const server = app.listen(config.PORT)
 
-;(() => {
+;(async () => {
     try {
+        // Database connection
+        const connection = await databaseService.connect()
+        logger.info(`Database Connection`, {
+            meta: {
+                CONNECTION_NAME: connection.name
+            }
+        })
+
         logger.info(`Application Info`, {
             meta: {
                 PORT: config.PORT,
